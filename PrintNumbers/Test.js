db8202e4209ab848fa14532a9a6d4137bb5a17fc
sudoaman <gupta.a@husky.neu.edu>
2017-03-16 13:29:20 -0400

    Create Test.js

---
+++ PrintNumbers/Test.js
@@ -0,0 +1,16 @@
+for (var i = 1; i<=100; i++)
+  {
+  	if (i % 5 === 0 && i % 3 === 0)
+  	{
+  		console.log("rightpoint");
+  	}
+
+else if (i % 3 === 0)
+{
+console.log("right");
+}else if ( i % 5 === 0)
+{
+console.log("point");
+}else
+console.log(i);
+}
